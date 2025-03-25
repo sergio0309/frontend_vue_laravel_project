00b416ebe8686bdb1a384b8072dead2de785a9a1
sergio0309 <sergio.rodriguezaramayo@gmail.com>
2025-03-24 21:58:26 -0400

    Proyecto Vue

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>App.Vue</h1>
+
+  <RouterLink to="/">Inicio</RouterLink> |
+  <RouterLink to="/nosotros">Nosotros</RouterLink> |
+  <RouterLink to="/servicios">Servicios</RouterLink> |
+  <RouterLink to="/blog">Blog</RouterLink> |
+  <RouterLink to="/auth/login">Ingresar</RouterLink> |
+  <br>
+  <RouterView/>
+  
+</template>
+
+<script setup>
+
+
+</script>
+
+<style scoped>
+
+</style>
